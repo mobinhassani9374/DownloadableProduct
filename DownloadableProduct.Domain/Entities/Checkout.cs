a3da5d463d373b67893c2ba2714762ef2f1571a8
mobinhassani9374 <mahdihassani9374@gmail.com>
2020-01-22 22:50:36 +0330

    AddEntity_Checkut_Migration

---
+++ DownloadableProduct.Domain/Entities/Checkout.cs
@@ -0,0 +1,14 @@
+﻿using DownloadableProduct.Domain.Enums;
+using System;
+
+namespace DownloadableProduct.Domain.Entities
+{
+    public class Checkout : BaseEntity
+    {
+        public long Price { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? ResponseDate { get; set; }
+        public CheckoutStatus Status { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ DownloadableProduct.Domain/Enums/CheckoutStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace DownloadableProduct.Domain.Enums
+{
+    public enum CheckoutStatus
+    {
+        Wating = 100,
+        Confirmed = 200,
+        Rejected = 300,
+    }
+}
